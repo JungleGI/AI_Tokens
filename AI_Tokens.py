92d406aae90a84488306d5780ce3ee3aa0571d92
JungleGI <154210017+JungleGI@users.noreply.github.com>
2023-12-22 09:30:34 +0100

    Add files via upload

--- AI_Tokens.py
+++ AI_Tokens.py
@@ -22,7 +22,7 @@ start_time = round((time.mktime(user_date.timetuple())))*1000
 end_time = round(time.mktime(datetime.datetime.now().timetuple()))*1000
 
 #Tickers
-tickers = ('9104','3911','2467','7533','22858','23756','16876')
+tickers = ('9104','3911','2467','7533','22858','23756','16876','15584','13051','24261')
 
 
 #Data
@@ -85,12 +85,12 @@ df_daily_returns = df_daily_returns[1:]
 df_cum_daily_returns = (1 + df_daily_returns).cumprod() - 1
 
 #Charting
-Chart1 = df_cum_daily_returns[['AIOZ Network','Artificial Liquid Intelligence','Ocean Protocol','OriginTrail','Dynex','ChainGPT','Oraichain']]
-#Chart2 = df_cum_daily_returns[[]]
+Chart1 = df_cum_daily_returns[['AIOZ Network','Artificial Liquid Intelligence','Ocean Protocol','OriginTrail','Dynex']]
+Chart2 = df_cum_daily_returns[['ChainGPT','Oraichain','Humans.ai','ARC','OctaSpace']]
 #Chart3 = df_cum_daily_returns[['Oasys','MOBOX','Aurory','Treasure','Nakamoto Games','Gods Unchained','Humans.ai','ARC''OctaSpace']]
 st.line_chart(Chart1)
-#st.line_chart(Chart2)
-#st.line_chart(Chart3),'15584''13051''24261'
+st.line_chart(Chart2)
+#st.line_chart(Chart3)
 
 
 
